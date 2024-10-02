118e12b3527ff8ecd94a5b3c03af7d4d1cd1de65
Akashtiwari9205 <144258529+Akashtiwari9205@users.noreply.github.com>
2024-10-02 13:23:00 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
+        <li>AWS,Azure</li>
+        <li>Devops</li>
         {/* <li>PostgreSQL</li>
         <li>Sequelize</li> */}
-        <li>JavaScript</li>
-        <li>React</li>
+        <li>Linux</li>
+        <li>Git/GitHub</li>
       </ul>
     ),
   },
@@ -56,16 +56,13 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={400} height={400} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express,
-             HTML, CSS, MongoDb, Mongoose, Git and Github. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+          Akash Tiwari - Founder, Shayam Traders
+
+          Akash Tiwari, founder of Shayam Traders, has 3 years of experience in AWS and DevOps, driving excellence in laptop sales and purchases.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
