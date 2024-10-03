7436d98f3838d4d3a865a517d0dd398263ec3a31
Akashtiwari9205 <144258529+Akashtiwari9205@users.noreply.github.com>
2024-10-03 09:26:04 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-             Learning never exhausts the mind :)
+             Passionate about leveraging AWS cloud solutions to optimize application performance and scalability
           </p>
           <div>
             <Link
